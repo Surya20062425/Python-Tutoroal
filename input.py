c444e5528dbf8369fbe7000302a31626406fc358
suryaprakash <b.7993974026@gmail.com>
2026-07-23 23:42:44 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,42 @@
+# we are taking the inputs from users 
+
+
+# by using input() method 
+
+
+name = input()
+
+# we are defined a variable name 
+
+
+# the input() function always takes input as string 
+
+
+
+# we can take input as integer 
+
+# By using int() function 
+
+# Before the input () method 
+
+
+age = int(input ())
+
+
+
+# we have assigned a variavle to take integer input 
+
+
+
+
+# we can also add a text inside input() method
+
+status = input(" enter your status ")
+
+
+
+#  I have created a variable that stores string as default 
+
+# It default shows as enter your status 
+
+
