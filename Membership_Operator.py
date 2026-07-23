616ff2f752e0d65cf4c7db8559ad5d3f67a76675
suryaprakash <b.7993974026@gmail.com>
2026-07-24 01:05:37 +0530

    Create Membership_Operator.py

---
+++ Membership_Operator.py
@@ -0,0 +1,17 @@
+x = 24
+y = 20
+
+# created and assigned the values to 2 vaeiables 
+my_list = [10, 20, 30, 40, 50] 
+
+  #  Created a array storing set of elements 
+
+if (x not in my_list): # checks the x element x exists in list or not 
+    print("x is NOT present in given list")
+else:
+    print("x is present in given list")
+
+if (y in my_list): # checking y existane in list
+    print("y is present in given list")
+else:
+    print("y is NOT present in given list")
