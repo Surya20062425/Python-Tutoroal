93fde8b46d4b75dbc57fd1ecba4bb223af1480ec
suryaprakash <b.7993974026@gmail.com>
2026-07-24 00:21:33 +0530

    Update variable.py

--- variable.py
+++ variable.py
@@ -35,3 +35,44 @@ print(f"Welcome {name} ")
 # we have implemented F-string method 
 
 # To work on realtime
+ name = "surya "
+
+name = input(" enter the name ")
+# python is a dynamic typing language 
+
+a = b = c = 100
+# we can assign a same value to different variables at a time 
+
+print(a, b, c)
+
+# at a same time different data-types
+# assigning at a time 
+
+x, y, z = 1, 2.5, "Python"
+print(x, y, z)
+
+
+# Deleting a variable 
+x = 10
+del x
+
+# we use del method to delete the variable 
+print(x)
+
+
+ # we can check the lenght of tge string 
+
+word = "Python"
+
+# created a string word 
+length = len(word)
+
+# we used len() to get lenght of word 
+print("Length of the word:", length)
+
+
+
+
+
+
+
