aa3797665b0b26d5f18890ec98be036032730346
suryaprakash <b.7993974026@gmail.com>
2026-07-24 00:59:20 +0530

    Create Logical_Operator.py

---
+++ Logical_Operator.py
@@ -0,0 +1,11 @@
+a = True
+b = False
+print(a and b)
+
+# if a & b both are true
+# Then it returns True
+# Else return False
+print(a or b) 
+# Atleast one in a & b is True
+# It  returns True 
+print(not a) # It returns the oposite value 
