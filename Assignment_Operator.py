a0b18a14baf074718dffbb2825a91ca865deb9a3
suryaprakash <b.7993974026@gmail.com>
2026-07-24 01:10:56 +0530

    Create Assignment_Operator.py

---
+++ Assignment_Operator.py
@@ -0,0 +1,13 @@
+a = 10
+b = a
+
+# equating a and b
+print(b)
+b += a # addiing the a value to the present value of a 
+print(b)
+b -= a
+print(b) # Decreasing the vlaue  of b by a 
+b *= a  # multiplying the b value by a 
+print(b)
+b <<= a # bitwise-left  operation by a to b 
+print(b)
