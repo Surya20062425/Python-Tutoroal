4a911fc9025841f4a925e4b550c014fe724b8e99
suryaprakash <b.7993974026@gmail.com>
2026-07-24 00:32:43 +0530

    Create Arthematic_Operator.py

---
+++ Arthematic_Operator.py
@@ -0,0 +1,59 @@
+ # by giving the static input 
+
+num_1 = 20
+
+# created a variable num_1 it stores a integer value 
+
+
+num_2 =10
+
+# It is an another variable num_2
+
+
+
+#  We are going to perform Arthematic operations 
+
+
+# Addition 
+print(a+b)
+
+# in another way 
+
+print("Addition ", a+b)
+
+# Substraction 
+
+print("Substraction", a-b)
+
+
+# Multiplicarion 
+
+print("mul ", a*b)
+# we use * symbol to perform the multiplication operation
+
+
+# Division  
+
+
+print("Division" , a/b)
+
+
+# floor division 
+
+
+print("floor_division" , a//b)
+
+# we have to use // for the floor division in python
+
+
+# modulus 
+
+print("modulus" , a%b) # we use % symbol for modulus operation
+
+
+# Exponential 
+
+
+print("exponentiaol " , a**b)   #  We use ** symbols for the exponential
+
+
