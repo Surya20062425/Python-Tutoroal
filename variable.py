43fd00b55c90665822eb22c9a8955cb7842e8cf5
suryaprakash <b.7993974026@gmail.com>
2026-07-24 00:00:54 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,37 @@
+  # we are going to create variables 
+
+
+name = " Surya "
+
+# I have created a variable name 
+# It stores string / characters
+
+
+age = 19 
+
+
+# we are created a variable that storing input as integer 
+
+
+
+# we are going to implement the input & output methods 
+
+
+# Created a welcome invitation 
+
+
+
+name = input(" Your name ")
+
+
+# we have created a variable name 
+
+
+# It stores the input taken by user 
+
+
+print(f"Welcome {name} ")
+
+# we have implemented F-string method 
+
+# To work on realtime
