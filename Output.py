a2b960d762ca6f142b7239ecf0228a319af4a2ca
suryaprakash <b.7993974026@gmail.com>
2026-07-23 23:52:13 +0530

    Create Output.py

---
+++ Output.py
@@ -0,0 +1,14 @@
+# Output is what we are displaying is called output 
+print(" hello")
+
+
+# we can display anything by using the print() method 
+
+# the methid print() is used to display the conten 
+
+
+print (19)
+
+# we can also print integer values by using print() method 
+
+
