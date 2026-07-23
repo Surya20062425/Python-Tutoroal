25cf82b60e3f00ac1b21f152dfa31c5e660ff976
suryaprakash <b.7993974026@gmail.com>
2026-07-24 01:02:33 +0530

    Create Identity_operator.py

---
+++ Identity_operator.py
@@ -0,0 +1,7 @@
+a = 10
+b = 20
+c = a
+# we are created 3 variables and assigned the values
+
+print(a is not b) # evaluating the condition  without conditional statement
+print(a is c) # it check both are same or not 
