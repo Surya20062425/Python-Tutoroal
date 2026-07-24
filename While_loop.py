882b111d6ab3ed50d05a8e9d967761d23ebc5373
suryaprakash <b.7993974026@gmail.com>
2026-07-24 13:28:29 +0530

    Update While_loop.py

--- While_loop.py
+++ While_loop.py
@@ -1,1 +1,14 @@
 
+cnt = 0
+while (cnt < 3):
+
+  # We use while loop when we dont know number of iterations 
+    cnt = cnt + 1
+
+  # the loop run upto cnt< 3
+    print("Hello Geek")
+
+while (True):
+
+  # The loop now runs based on the condition 
+    print("Hello Geek")
