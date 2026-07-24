e62b06b6bc21600c36cc4e1d68db047b1ee7bf4f
suryaprakash <b.7993974026@gmail.com>
2026-07-24 13:06:52 +0530

    Create Boolean.py

---
+++ Boolean.py
@@ -0,0 +1,17 @@
+# we know that True and False are the boolean values
+
+print(type(True))
+
+# Checking the data-type by type() method 
+print(type(False))
+
+if 1:
+
+  # the 1 is true
+    print("1 is truthy")
+
+if not 0:
+
+  # 0 indicates false
+    print("0 is falsy")
+  
