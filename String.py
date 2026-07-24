6a8d4ba6e7753803fd31c692c5f09d8f4657f973
suryaprakash <b.7993974026@gmail.com>
2026-07-24 12:59:30 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,12 @@
+s = 'Welcome to the Geeks World'
+
+# the string that have been divided by 5 
+
+# gaps separates the string 
+print(s)
+print(type(s))
+
+# access string with index
+print(s[1])
+# we can check the string elements by using index number 
+print(s[-1])
