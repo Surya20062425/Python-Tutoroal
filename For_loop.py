5e722d7390db9e08b87c687dd9119c785d510fde
suryaprakash <b.7993974026@gmail.com>
2026-07-24 13:24:46 +0530

    Create For_loop.py

---
+++ For_loop.py
@@ -0,0 +1,21 @@
+n = 4
+
+# we use for loop when we know the number of iterations 
+for i in range(0, n):
+
+  # the looop executes from 0 to 4/n- value  
+  # Because  we assigned the n value to 4
+    print(i)
+
+
+a = ["geeks", "for", "geeks"]
+
+# created a list containing list of elements 
+for idx in range(len(a)):
+
+  # printing the elements upto the lenght of the list 
+
+  # lenght start from 1
+
+  # Index start from 0
+    print(a[idx])
