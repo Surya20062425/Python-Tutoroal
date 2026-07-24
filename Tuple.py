e4f6337b3995a57bf016ccdf060905eec48fa789
suryaprakash <b.7993974026@gmail.com>
2026-07-24 13:04:12 +0530

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,10 @@
+t1 = (1,)
+# we created a list
+print(type(t1))
+
+t2 = ('Geeks', 'For', 'Geeks', 1, 2)
+# Created a  list with 5 elements 
+print(t2[3])
+
+#  Printing the elements at the indexes 
+print(t2[-3])
