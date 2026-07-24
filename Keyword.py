5b8eaf26cae28933377e30e03a7e448b0b8b4fde
suryaprakash <b.7993974026@gmail.com>
2026-07-24 12:53:15 +0530

    Create Keyword.py

---
+++ Keyword.py
@@ -0,0 +1,19 @@
+import keyword 
+
+# import is the keyword 
+
+# That was used to import a package / module into the file 
+
+
+# In above step we have used keyword 
+
+# keyword is package contains all the keywords in it 
+
+
+print(keyword.kwlist)
+
+# The keyword kwlist is used to fetch the list of elements from package keyword
+
+
+# In above step we are fetching the data from the keyword package
+
