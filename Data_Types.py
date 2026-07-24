7f1c997dfaae2488eaad95df529ce762c964b053
suryaprakash <b.7993974026@gmail.com>
2026-07-24 12:57:28 +0530

    Create Data_Types.py

---
+++ Data_Types.py
@@ -0,0 +1,28 @@
+x = 50
+
+#int 
+x = 60.5
+# float
+x = "Hello World"
+# string
+x = ["geeks", "for", "geeks"]
+
+# array 
+x = ("geeks", "for", "geeks")
+# list 
+
+
+a = 5
+# int
+b = 5.0
+# float
+c = 2 + 4j
+# complex 
+print(type(a))
+# we can check the datatype of variable by using type ()
+print(type(b))
+# Type() checks the data-type
+print(type(c)) 
+
+
+
