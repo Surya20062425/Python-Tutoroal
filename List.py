1e433304cf3d46d29198e15d5798f6a524835fa0
suryaprakash <b.7993974026@gmail.com>
2026-07-24 13:01:48 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,10 @@
+a = [1, 2, 3]
+
+# We defined a list a
+print(a)
+
+b = ["Geeks", "For", "Geeks", 4, 5]
+
+# Defined a list with different data-types 
+print(b[3]) # printing element at index 3
+print(b[-3])# printing element at index -3
