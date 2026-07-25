2fb50fdb3e1c198e1184e5305d775cab23d7c7f5
suryaprakash <b.7993974026@gmail.com>
2026-07-25 22:28:59 +0530

    Create define_functon.py

---
+++ define_functon.py
@@ -0,0 +1,36 @@
+# creating a function
+# The key-word def is used to define a function
+
+# def function_name (parameter): # function_name can be assigned any name 
+  # the above is the defining a function method 
+
+  # we can give any name for our function 
+
+  # except the reserved keywords 
+
+  # we have to pass the parameters through the function to perform the task
+
+  # usually we use the function for recursion process 
+
+
+# creating a function to print the string 
+
+def printing () :
+  print("hello")
+
+# the above function is a simple syntax
+# We have used the function without any parameters 
+# Due to we dont need any parameter to print 
+
+number= 12
+
+# created a variable a 
+
+# assigned number value 
+def print_a(number):  # used a as a parameter to print number
+  print(a)
+
+
+# the above function simply prints  the value stored  in a
+
+  
