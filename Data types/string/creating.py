deeee201be676827dee50889b1575e8a60bd4c77
suryaprakash <b.7993974026@gmail.com>
2026-08-02 10:56:24 +0000

    Initial commit

---
+++ Data types/string/creating.py
@@ -0,0 +1,17 @@
+str = "my string"
+
+# we have created  a variable str 
+
+# note : The string always enclosed by doble or single quotes
+str=  "surya "
+
+# we know tha the  python is dynamic 
+
+# the value of string is changed
+s = """I am Learning
+Python String""" # we have used  triple quotes for multi line 
+print(s)
+
+s = '''I'm a 
+Geek'''
+print(s)
